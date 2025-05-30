1c0f4ceafc9e286e9d31658a42960fb25d7156b3
Addisalem-chane <addisalemchane19@gmail.com>
2025-05-29 16:32:38 -0800

    added base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/amazon-clone/",
+  base: "/amazon-clone",
   server: {
     open: true,
     port: 3001,
